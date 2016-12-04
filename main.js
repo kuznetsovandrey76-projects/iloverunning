49b856b15fca34c354ab346950775ec9a7a9f829
kuznetsovandrey76 <and.rey.q@yandex.ru>
2016-12-04 12:10:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+var lesson = [
+		["Триатлон без травм. Владимир Демченко", "111", "https://iloverunningchannel.vhx.tv/videos/vladimir-demchenko-triathlon"], 
+		["Бегущая по небу. Наталья Нещерет", "97", "https://iloverunningchannel.vhx.tv/videos/natalia-nesheret-begushaia-po-nebu-lookatme-cat"],
+		["Бег и сердце. Встреча 2. Симон Мацкеплишвили", "121", "https://iloverunningchannel.vhx.tv/videos/simon-vstr-2"], 
+		["Триатлон изнутри. Дмитрий Бутков", "181", "https://iloverunningchannel.vhx.tv/videos/dmitri-butkov-triatlon-iznutri"], 
+		["Правильное педалирование. Ян Бравый и Евгений Корнеев.", "61", "https://iloverunningchannel.vhx.tv/videos/pedalirovanie"], 
+		["Победить пустыню. Наталья Седых", "86", "https://iloverunningchannel.vhx.tv/videos/mds-natalya-sedih"], 
+		["Бег и сердце. Симон Мацкеплишвили", "123", "https://iloverunningchannel.vhx.tv/videos/cardio"], 
+		["Роман Авдеев. Невозможное возможно", "71", "https://iloverunningchannel.vhx.tv/videos/roman-avdeev"], 
+		["Реабилитация после травм. Александрина Тиль", "80", "https://iloverunningchannel.vhx.tv/videos/alexandrina-til-reabilitation"], 
+		["Александр Сорокин. Спортивное питание", "65", "https://iloverunningchannel.vhx.tv/videos/pitanie"], 
+		["100 марафонов, наука и жизнь.", "95", "https://iloverunningchannel.vhx.tv/videos/100"], 
+		["Идеальный вес. Марко Кошир", "110", "https://iloverunningchannel.vhx.tv/videos/marco-koshier-ideal-weight"], 
+		["Спорт в Норвегии. Как это устроено. Михаил Виноградов", "93", "https://iloverunningchannel.vhx.tv/videos/sport-in-norway-1"], 
+		["Метод GTD. Как успевать сделать и то, что нужно и то, что хочется", "125", "https://iloverunningchannel.vhx.tv/videos/gtd"], 
+		["Михаил Иванов. Друг приехал из Америки", "88", "https://iloverunningchannel.vhx.tv/videos/michael-ivanov"],
+		["Диета для атлета. Евгений Швец", "74", "https://iloverunningchannel.vhx.tv/videos/diet-for-athletes"],
+		["Правильное планирование года в триатлоне. Марко Кошир", "103", "https://iloverunningchannel.vhx.tv/videos/pravilnoe-planirovane-goda-marco-koshier"],
+		["Москва-Владивосток. Экстрим на пределе", "111", "https://iloverunningchannel.vhx.tv/videos/moscow-vladivostok"],
+		["Йога для бегунов. Сергей Сидоренко", "130", "https://iloverunningchannel.vhx.tv/videos/yoga-for-runners"],
+		["Ева Угляница. Основы правильного питания для атлетов. Встреча 2", "70", "https://iloverunningchannel.vhx.tv/videos/eva2"],
+		["Массаж и физические нагрузки. Роман Массальский", "101", "https://iloverunningchannel.vhx.tv/videos/massage"], 
+		["Ева Угляница. Основы правильного питания для атлетов", "82", "https://iloverunningchannel.vhx.tv/videos/evauglanitsa-p1"],
+		["Марко Кошиер. Низкоуглеводная диета. Часть 2", "88", "https://iloverunningchannel.vhx.tv/videos/marcokoschier-1"],
+		["Михаил Сорокин. Суперроллер", "46", "https://iloverunningchannel.vhx.tv/videos/superroller"],
+		["Сергей Агапкин. Беговые диеты", "115", "https://iloverunningchannel.vhx.tv/videos/sergeyagapkin"], 
+		["Юлия Вознесенская. Быль и небыль о мужчинах и женщинах", "103", "https://iloverunningchannel.vhx.tv/videos/juliavoznesenskaya"], 
+		["Алексей Акатьев. Особенности плавания на открытой воде", "82", "https://iloverunningchannel.vhx.tv/videos/alexeyakatiev"], 
+		["Семинар с веломехаником", "112", "https://iloverunningchannel.vhx.tv/videos/velomechanik"], 
+		["Правильная велопосадка", "115", "https://iloverunningchannel.vhx.tv/videos/veloposadka"], 
+		["Сергей Медведев. Спорт как социальное явление XX века", "97", "https://iloverunningchannel.vhx.tv/videos/smedvedev"], 
+		["Владимир Демченко. Все о коленях, стопах и связках", "93", "https://iloverunningchannel.vhx.tv/videos/vladimirdemchenko"], 
+		["Оборудование и экипировка в велоспорте", "118", "https://iloverunningchannel.vhx.tv/videos/oborudovaniyedlyavelosporta"], 
+		["Дмитрий Ерохин. О приоритетах в любимых делах и спорте", "107", "https://iloverunningchannel.vhx.tv/videos/dmitryerohin"], 
+		["Сергей Медведев. О спорте, жизни и мечте", "102", "https://iloverunningchannel.vhx.tv/videos/sergeymedvedev"], 
+		["Николай Романов. О спорте, правильном беге и победах", "118", "https://iloverunningchannel.vhx.tv/videos/nikolayromanov"], 
+		["Алексей Комиссаров. О беге, целях, силе воли, вдохновении и Байкальском марафоне!", "83", "https://iloverunningchannel.vhx.tv/videos/a-komissarov-1"], 
+		["Андрей Волков. О высоких целях, спорте и вдохновении", "93", "https://iloverunningchannel.vhx.tv/videos/andrey-volkov"],
+		["Крисси Веллингтон. О спорте, мотивации и жизни", "123", "https://iloverunningchannel.vhx.tv/videos/chrissie-wellington"],
+		["Андрей Прокудин. Спорт как внутренняя игра", "85", "https://iloverunningchannel.vhx.tv/videos/andrey-prokudin"],
+		["Лекторий. Леонид Богуславский. О целях, спорте и победах.", "80", "https://iloverunningchannel.vhx.tv/videos/l-boguslavsky"],
+		["Марко Кошиер. Низкоуглеводная диета. Часть 1", "95", "https://iloverunningchannel.vhx.tv/videos/marco-coshier"],
+		["Велоспорт: как это устроено", "123", "https://iloverunningchannel.vhx.tv/videos/velosport"]
+	];
+	// var total = 0;
+	// function sum() {
+	// 	for(var i = 0; i < lesson.length; i++) {
+	// 		total += parseInt(lesson[i][1]);
+	// 		}
+	// 	return total;
+	// 	}
+
+	// total = sum();
+	// var allTime = ((total-(total%60))/60+"h "+(total%60)+"m");
+	var listLesson = "<ol>";
+	function list() {
+		for(var i = 0; i < lesson.length; i++) {
+			listLesson += "<li><a href=\""+lesson[i][2]+"\">"+lesson[i][0]+"</a></li>";
+	} 
+	return listLesson;
+}
+document.write(list());
